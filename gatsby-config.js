b1152842f1781691b2421c1dc9c85293bf92d9b1
Oleg Loutchansky <luchansodev@gmail.com>
2022-06-15 22:46:57 +0300

    feat: add chakra ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,21 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
