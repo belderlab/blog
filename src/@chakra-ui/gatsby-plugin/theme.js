c71ad2835eab212fc13bc2876a81a015bfc8c516
Oleg Loutchansky <luchansodev@gmail.com>
2022-06-21 23:46:47 +0300

    fix: remove unused styles

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,13 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
 const theme = {
-  styles: {
-    global: (props) => ({
-      body: {
-        bg: "red.500",
-      },
-    }),
-  },
-
   config: {
     disableTransitionOnChange: false,
   },
